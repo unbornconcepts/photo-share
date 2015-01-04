7409f87cc806afca8ca872a88377a030e0afb9be
Nick Nance <nance.nick@gmail.com>
2015-01-04 13:52:06 -0600

    additional cordova tasks

--- gulpfile.js
+++ gulpfile.js
@@ -191,10 +191,10 @@ var fs = require('fs');
 var path = require('path');
 var cordova = require('cordova-lib').cordova.raw; // promises API
 var buildDir = path.join(__dirname, 'build');
+var platforms = ['ios', 'android'];
 var plugins = ['org.apache.cordova.file','com.ionic.keyboard','org.apache.cordova.statusbar'];
-var platform_dirs = ['node_modules/cordova-ios', 'node_modules/cordova-android'];
 
-gulp.task('cordova:clean', del.bind(null, ['build']));
+gulp.task('cordova:clean', del.bind(null, ['build','platforms','plugins']));
 
 gulp.task('cordova:create', ['cordova:clean'], function() {
     fs.mkdirSync(buildDir);
@@ -203,16 +203,26 @@ gulp.task('cordova:create', ['cordova:clean'], function() {
     fs.symlinkSync(path.join('..', 'config.xml'), 'config.xml');
     fs.symlinkSync(path.join('..', 'www'), 'www');
 
-    return cordova.plugins('add', plugins)
-    .then(function() {
-        // point to node_modules/cordova-android/
-        return cordova.platform('add', platform_dirs);
-    });
+    return cordova.plugins('add', plugins);
+});
+
+platforms.forEach(function(platform){
+  gulp.task('cordova:platform:' + platform, function() {
+    process.chdir(buildDir);
+    return cordova.platform('add', platform);
+  });
 });
 
 gulp.task('cordova:build', function() {
     process.chdir(buildDir);
     return cordova.build();
+});
+
+platforms.forEach(function(platform){
+  gulp.task('cordova:build:' + platform, function() {
+    process.chdir(buildDir);
+    return cordova.build(platform);
+  });
 });
 
 // Load custom tasks from the `tasks` directory
